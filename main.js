f015260d7637a983cc7b0acf20f96172b35289f3
Aishu203100 <79292874+Aishu203100@users.noreply.github.com>
2021-03-04 13:57:03 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function nextscreen() {
+   document.getElementById("screen1").style.display='none';
+    document.getElementById("screenhome").style.display='block';
+}
+function homescreenload(){
+  document.getElementById("screen1").style.display='none';
+  document.getElementById("screenhome").style.display='block';
+}
+function screen1trans(){
+  document.getElementById("screen1").style.display='block';
+    document.getElementById("screenhome").style.display='none';
+}
